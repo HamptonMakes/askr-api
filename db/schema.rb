edaf0e971ef20698652fff3d31f8057f131b2fe8
Hampton Catlin <hcatlin@greed.local>
2008-07-29 15:19:38 -0400

    fixing jQuery

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 6) do
+ActiveRecord::Schema.define(:version => 20080729191848) do
 
   create_table "answers", :force => true do |t|
     t.string   "content"
