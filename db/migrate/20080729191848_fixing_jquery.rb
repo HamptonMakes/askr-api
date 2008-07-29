92b7863b3954409e693da433ead448214de5f1cb
Hampton Catlin <hcatlin@greed.local>
2008-07-29 16:41:48 -0400

    adding in religion quesiton

--- db/migrate/20080729191848_fixing_jquery.rb
+++ db/migrate/20080729191848_fixing_jquery.rb
@@ -1,6 +1,6 @@
 class FixingJquery < ActiveRecord::Migration
   def self.up
-    Answer.find_by_content("JQuery").update_attributes(:content => "jQuery")
+    #Answer.find_by_content("JQuery").update_attributes(:content => "jQuery")
   end
 
   def self.down
