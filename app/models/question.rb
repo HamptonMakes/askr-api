29d618f69a5e68ee422d4b14b082fa6c4afcc49c
Evan Light <evan@tripledogdare.net>
2014-04-08 19:48:26 -0400

    Aaaand this is what I really wanted.
    It just didn't seem right to :include => :answers from within
    the Question model.
    
    Boy, I am glad this is not open sourced.... ;-)

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,10 +2,6 @@ class Question < ActiveRecord::Base
   has_many :answers, :order => "id ASC"
   has_many :votes
   
-  def self.before_date(date)
-    all(:conditions => ["created_at < ?", date])
-  end
-
   def votes_in(year)
     self.votes.count(:conditions => {:year => year})
   end
