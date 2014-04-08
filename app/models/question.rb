3fc4f61d668d43c41ad6c4f7fc431afeea2cf357
Evan Light <evan@tripledogdare.net>
2014-04-08 19:43:23 -0400

    Adds before_date query to Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,10 @@ class Question < ActiveRecord::Base
   has_many :answers, :order => "id ASC"
   has_many :votes
   
+  def before_date(date)
+    self.all(:conditions => ["created_at < ?", date])
+  end
+
   def votes_in(year)
     self.votes.count(:conditions => {:year => year})
   end
