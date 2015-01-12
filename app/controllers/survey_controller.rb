038130e6fc90a5bfdd46d9656d71c4932516198e
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-01-12 14:38:48 -0800

    make the year dynamic

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -3,23 +3,23 @@ class SurveyController < ApplicationController
 
     @person = Person.new(:email_address => params[:person][:email_address]) if params[:person]
     @questions = Question.find(:all, :include => [:answers])
-    
+
     if request.post? && params[:question]
       @person.ip_address = request.remote_ip
       @person.user_agent = request.headers["HTTP_USER_AGENT"]
       @person.save
 
       params[:question].each do |key, value|
-        Vote.create(:question_id => key, :answer_id => value, :year => 2012, :person => @person)
+        Vote.create(:question_id => key, :answer_id => value, :year => Time.now.year, :person => @person)
       end
 
       redirect_to :action => "finished"
     end
   end
-  
+
   def stats
   end
-  
+
   def emails
     #@people = Person.find(:all)
   end
