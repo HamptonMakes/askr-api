bf40dd86712244396c4f2c65b1ef536b91c71158
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-01-15 13:47:58 -0800

    heyyo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   def end_time
     @end_time ||= Date.parse("Feb 15, 2015").to_time
   end
+  helper_method :end_time
 
   def results_year
     2014
