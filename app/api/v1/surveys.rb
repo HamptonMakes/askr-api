85df6cf5f3f4c7871e10eea7578bc8d020a246e1
Hampton Catlin <hcatlin@gmail.com>
2015-03-26 11:45:47 -0400

    have to include directly in controller

--- app/api/v1/surveys.rb
+++ app/api/v1/surveys.rb
@@ -2,11 +2,14 @@ module V1
   class Surveys < Grape::API
     include V1::Defaults
 
-    resources :surveys, :serializer => SurveySerializer do
+    include Grape::Rails::Cache
+
+    resources :surveys do
       desc "grab a survey"
       get '/:name' do
+        content_type 'application/javascript'
         survey = Survey.where(:slug => params[:name]).first
-        cache(key: "survey:#{survey.id}", etag: survey.updated_at, expires_in: 2.days) do
+        cache(key: "survey::#{survey.id}", etag: survey.updated_at, expires_in: 2.days) do
           survey
         end
       end
