cee617177e0135ebc82378f6a77289ed1f49b57a
Hampton Catlin <hcatlin@gmail.com>
2015-03-26 10:33:20 -0400

    no uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,10 +22,6 @@ RubySurvey::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
